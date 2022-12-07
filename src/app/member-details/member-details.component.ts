fd9cd8352e05ec7c1535493278312ed5f2d3cf7a
MohamedShokrani <MohamedShokrani55@gmail.com>
2022-12-07 14:06:17 +0200

    add member details and gallery for every member

--- src/app/member-details/member-details.component.ts
+++ src/app/member-details/member-details.component.ts
@@ -1,24 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { Member } from '../_Models/memebr';
 import { MembersService } from '../_services/members.service';
 
 @Component({
   selector: 'app-member-details',
   templateUrl: './member-details.component.html',
-  styleUrls: ['./member-details.component.css']
+  styleUrls: ['./member-details.component.css'],
+
 })
+
 export class MemberDetailsComponent implements OnInit {
-  member:Member
-  constructor(private memberService:MembersService,private route:ActivatedRoute) { }
+  member: Member
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.getMemberDetailsByname()
-  }
+    this.galleryOptions = [{
+      width: '500px',
+      height: '500px',
+      imagePercent: 100,
+      thumbnailsColumns: 4,//maximum four images underneath the image
+      imageAnimation: NgxGalleryAnimation.Slide,
+      preview: false
 
-  getMemberDetailsByname(){
-    this.memberService.GetMember(this.route.snapshot.paramMap.get('username')).subscribe(member=>{
-      this.member = member
+    }
+
+    ]
+   
+
+  }
+  getImages(): NgxGalleryImage[] {
+    const imagesUrls = [];
+    for (const img of this.member.photos) {
+      imagesUrls.push({
+        small: img?.url,
+        medium: img?.url,
+        big: img?.url,
+
+
+      })
+
+    }
+    return imagesUrls
+  }
+  getMemberDetailsByname() {
+    this.memberService.GetMember(this.route.snapshot.paramMap.get('username')).subscribe(member => {
+      this.member = member;
+      this.galleryImages = this.getImages();
     })
   }
 
